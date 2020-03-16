e6c435ca5415378007f853a37e668a2d4f99b092
HiroakiSHA <l0ck.h1r0ak1@gmail.com>
2020-03-17 01:17:22 +0900

    adjust rename py

--- UE4/UE4.23/Python/sha_rename.py
+++ UE4/UE4.23/Python/sha_rename.py
@@ -1,4 +1,5 @@
 import unreal
+import sys
 
 def rename_asset(search_str='', replacement_str=''):
     '''replace search_str to replacement_str
@@ -19,7 +20,7 @@ def rename_asset(search_str='', replacement_str=''):
         unreal.EditorAssetLibrary.rename_asset(package_name, new_name_path)
 
 
-def add_prefix_suffix_to_asset(prefix='', suffix=''):
+def add_prefix(prefix=''):
     asset_list = unreal.EditorUtilityLibrary.get_selected_asset_data()
     for asset in asset_list:
         asset_name = asset.asset_name
@@ -27,7 +28,32 @@ def add_prefix_suffix_to_asset(prefix='', suffix=''):
         package_name = asset.package_name
         package_path = asset.package_path
 
-        new_name_path = str(package_path) + '/' + prefix + str(asset_name) + suffix
+        new_name_path = str(package_path) + '/' + prefix + str(asset_name)
 
         #TODO : fix already an asset with same name.
         unreal.EditorAssetLibrary.rename_asset(package_name, new_name_path)
+
+
+def add_suffix(suffix=''):
+    asset_list = unreal.EditorUtilityLibrary.get_selected_asset_data()
+    for asset in asset_list:
+        asset_name = asset.asset_name
+        object_path = asset.object_path
+        package_name = asset.package_name
+        package_path = asset.package_path
+
+        new_name_path = str(package_path) + '/' + str(asset_name) + suffix
+
+        #TODO : fix already an asset with same name.
+        unreal.EditorAssetLibrary.rename_asset(package_name, new_name_path)
+
+args = sys.argv
+
+if args[1] == 'r':
+    rename_asset(args[2], args[3])
+
+if args[1] == 'p':
+    add_prefix(args[2])
+
+if args[1] == 's':
+    add_suffix(args[2])
