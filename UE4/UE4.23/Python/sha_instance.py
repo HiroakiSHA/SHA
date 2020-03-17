d64fa95cf9bbad2e2bbb4834420794e54b504cbf
HiroakiSHA <l0ck.h1r0ak1@gmail.com>
2020-03-17 23:39:31 +0900

    wip reverse instance python

--- UE4/UE4.23/Python/sha_instance.py
+++ UE4/UE4.23/Python/sha_instance.py
@@ -1,20 +1,24 @@
 import unreal
 
-actors = unreal.EditorLevelLibrary.get_selected_level_actors()
+def reverse_instance():
+    actors = unreal.EditorLevelLibrary.get_selected_level_actors()
 
-for actor in actors:
-    instanced_static_mesh = actor.root_component
-    count = instanced_static_mesh.get_instance_count()
-    static_mesh = instanced_static_mesh.static_mesh
-    for num in range(count):
-        location = instanced_static_mesh.get_instance_transform(num).translation
-        rotation = instanced_static_mesh.get_instance_transform(num).rotation.rotator()
-        scale = instanced_static_mesh.get_instance_transform(num).scale3d
-        new_actor = unreal.EditorLevelLibrary.spawn_actor_from_object(static_mesh, location, rotation)
-        new_actor.set_actor_scale3d(scale)
+    for actor in actors:
+        root_component = actor.root_component
+        all_component = root_component.get_children_components(False)
+        all_component.append(root_component)
 
-    unreal.log(static_mesh)
-    option = unreal.EditorScriptingJoinStaticMeshActorsOptions()
-    
 
-unreal.log('------------------------')
+        for ism in all_component:
+            count = ism.get_instance_count()
+            static_mesh = ism.static_mesh
+            for num in range(count):
+                location = ism.get_instance_transform(num).translation
+                rotation = ism.get_instance_transform(num).rotation.rotator()
+                scale = ism.get_instance_transform(num).scale3d
+                new_actor = unreal.EditorLevelLibrary.spawn_actor_from_object(static_mesh, location, rotation)
+                new_actor.set_actor_scale3d(scale)
+
+        option = unreal.EditorScriptingJoinStaticMeshActorsOptions()
+
+reverse_instance()
